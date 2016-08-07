f33bbaa6bb057921207e9c12e3e266816b1e86c5
aidan <aidankrtz@gmail.com>
2016-08-07 08:47:44 -0700

    Framefield propagation done.

--- source/framefield.py
+++ source/framefield.py
@@ -13,9 +13,10 @@ from utils import tet_centroid
 
 import math
 import numpy as np
+from scipy import spatial
 
 class Frame(object):
-    def __init__(self, u, v, w, location):
+    def __init__(self, u = [], v = [], w = [], location = []):
         self.u, self.v, self.w = u, v, w
         self.location = location
 
@@ -28,19 +29,27 @@ def init_framefield(tet_mesh, surf_mesh):
         ti = tet_mesh.adjacent_elements[surf_mesh.face_map.inv[fi]][0]
         tet = tet_mesh.elements[ti]
         # Ignore faces which have 0 curvature.
-        if math.isclose(surf_mesh.k1[surf_face[0]], 0) and \
-           math.isclose(surf_mesh.k2[surf_face[0]], 0):
+        if math.isclose(surf_mesh.k1[surf_face[0]], 0) and math.isclose(surf_mesh.k2[surf_face[0]], 0):
             continue
         # @TODO(aidan) Find actual face values, not vertex.
-        frame = Frame( surf_mesh.pdir1[surf_face[0]],
-                        surf_mesh.pdir2[surf_face[0]],
-                        surf_mesh.vertex_normals[surf_face[0]],
-                        tet_centroid(tet_mesh, ti) )
-        boundary_frames.append(frame)
-    
-    plot_framefield(boundary_frames)
+        boundary_frames.append(Frame( surf_mesh.pdir1[surf_face[0]],
+                                      surf_mesh.pdir2[surf_face[0]],
+                                      surf_mesh.vertex_normals[surf_face[0]],
+                                      tet_centroid(tet_mesh, ti) ))
 
+    # Prepare a KDTree of boundary frame coords for quick spatial queries.
+    tree = spatial.KDTree(np.vstack(map(lambda frame: frame.location, boundary_frames)))
+
+    frames = []
     # Now propagate the boundary frames throughout the tet mesh.
-    # for ti, tet in enumerate(tet_mesh.elements):
-    #     location = tet_centroid(tet_mesh, ti)
+    for ti, tet in enumerate(tet_mesh.elements):
+        location = tet_centroid(tet_mesh, ti)
+        nearest_ti = tree.query(location)[1] # Find closest boundary frame
+        # Create new frame
+        frames.append(Frame( boundary_frames[nearest_ti].u,
+                             boundary_frames[nearest_ti].v,
+                             boundary_frames[nearest_ti].w,
+                             location ))
 
+        
+    plot_framefield(frames)
